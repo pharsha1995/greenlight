4269666c4ce954fdb78823af9494c7ee5903da77
harsha <pharshavardhan2@gmail.com>
2024-06-07 12:50:40 +0530

    add code for updating and deleting movie

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,8 +6,11 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
+
 	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
+	mux.HandleFunc("PUT /v1/movies/{id}", app.updateMovieHandler)
+	mux.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
 
 	return app.recoverPanic(mux)
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -69,9 +69,39 @@ func (m *MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m *MovieModel) Update(movie *Movie) error {
-	return nil
+	stmt := `UPDATE movies
+	         SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+					 WHERE id = $5
+					 RETURNING version`
+
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(stmt, args...).Scan(&movie.Version)
 }
 
 func (m *MovieModel) Delete(id int64) error {
+	stmt := `DELETE FROM movies
+	         WHERE id = $1`
+
+	result, err := m.DB.Exec(stmt, id)
+	if err != nil {
+		return err
+	}
+
+	rowAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowAffected == 0 {
+		return ErrNoRecord
+	}
+
 	return nil
 }
