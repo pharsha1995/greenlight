c72ee5dcaf9a406afaa1e73c0962e2dfbd98a347
harsha <pharshavardhan2@gmail.com>
2024-06-07 14:53:43 +0530

    allow partial updating of movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -94,9 +94,9 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
+		Title   *string  `json:"title"`
+		Year    *int32   `json:"year"`
+		Runtime *int32   `json:"runtime"`
 		Genres  []string `json:"genres"`
 	}
 
@@ -106,10 +106,21 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	v := validator.New()
 
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,7 +9,7 @@ func (app *application) routes() http.Handler {
 
 	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
-	mux.HandleFunc("PUT /v1/movies/{id}", app.updateMovieHandler)
+	mux.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
 	mux.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
 
 	return app.recoverPanic(mux)
