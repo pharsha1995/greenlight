8128f169f057973bd36c6e5a5b30550b03043c11
harsha <pharshavardhan2@gmail.com>
2024-06-08 16:19:03 +0530

    add UserModel for db ops with users

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,12 +1,17 @@
 package data
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"time"
 
 	"github.com/pharsha1995/greenlight/internal/data/validator"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var ErrDuplicateEmail = errors.New("users: duplicate email")
+var emailUniquePQErrMsg = `pq: duplicate key value violates unique constraint "users_email_key"`
 
 type password struct {
 	plaintext *string
@@ -59,4 +64,89 @@ func ValidateUser(v *validator.Validator, user *User) {
 	if user.Password.hash == nil {
 		panic("missing password hash for user")
 	}
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m *UserModel) Insert(user *User) error {
+	stmt := `INSERT INTO users (name, email, password_hash, activated)
+					 VALUES ($1, $2, $3, $4)
+					 RETURNING id, created_at, version`
+
+	args := []any{user.Name, user.Email, user.Password.hash, user.Activated}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, stmt, args...).Scan(&user.ID, &user.CreateAt, &user.Version)
+	if err != nil {
+		if err.Error() == emailUniquePQErrMsg {
+			return ErrDuplicateEmail
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (m *UserModel) GetByEmail(email string) (*User, error) {
+	stmt := `SELECT id, created_at, name, email, password_hash, activated, version
+					 FROM users
+					 WHERE email = $1`
+
+	user := User{}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, stmt, email).Scan(
+		&user.ID,
+		&user.CreateAt,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (m *UserModel) Update(user *User) error {
+	stmt := `UPDATE users
+	         SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1
+					 WHERE id = $5 AND version = $6
+					 RETURNING version`
+
+	args := []any{
+		user.Name,
+		user.Email,
+		user.Password.hash,
+		user.Activated,
+		user.ID,
+		user.Version,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, stmt, args...).Scan(&user.Version)
+	if err != nil {
+		if err.Error() == emailUniquePQErrMsg {
+			return ErrDuplicateEmail
+		} else if errors.Is(err, sql.ErrNoRows) {
+			return ErrNoRecord
+		}
+		return err
+	}
+
+	return nil
 }
