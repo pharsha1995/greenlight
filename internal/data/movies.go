4562081d053d8b3f7ea74a4d8c10ea291ca8074e
harsha <pharshavardhan2@gmail.com>
2024-06-08 11:12:20 +0530

    add pagination and metadata for it

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -129,27 +129,33 @@ func (m *MovieModel) Delete(id int64) error {
 	return nil
 }
 
-func (m *MovieModel) GetAll(title string, genres []string, filters *Filters) ([]*Movie, error) {
-	stmt := fmt.Sprintf(`SELECT id, created_at, title, year, runtime, genres, version
-	         FROM movies
-					 WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
-					 AND (genres @> $2 OR $2 = '{}')
-					 ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+func (m *MovieModel) GetAll(title string, genres []string, filters *Filters) ([]*Movie, *Metadata, error) {
+	stmt := fmt.Sprintf(`
+	        SELECT count(*) OVER(), id, created_at, title, year, runtime, genres, version
+	        FROM movies
+					WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+					AND (genres @> $2 OR $2 = '{}')
+					ORDER BY %s %s, id ASC
+					LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
+
+	args := []any{title, pq.Array(genres), filters.limit(), filters.offset()}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, stmt, title, pq.Array(genres))
+	rows, err := m.DB.QueryContext(ctx, stmt, args...)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	defer rows.Close()
 
+	totalRecords := 0
 	movies := []*Movie{}
 	for rows.Next() {
 		movie := Movie{}
 		err := rows.Scan(
+			&totalRecords,
 			&movie.ID,
 			&movie.CreatedAt,
 			&movie.Title,
@@ -159,15 +165,15 @@ func (m *MovieModel) GetAll(title string, genres []string, filters *Filters) ([]
 			&movie.Version,
 		)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		movies = append(movies, &movie)
 	}
 
 	if err = rows.Err(); err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return movies, nil
+	return movies, calculateMetadata(totalRecords, filters.Page, filters.PageSize), nil
 }
